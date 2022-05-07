cc6ba9c3dfb2ea7cbf85bc042505ff169af5e00f
Radhika <radhika.parkhi@cctech.co.in>
2022-05-07 10:26:53 +0530

    refactored code.

--- longest_string.c
+++ longest_string.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char* argv[]) {
-    if (argc == 1)
-        printf("");
-     else if(argc==2)
-         printf(argv[1]);
 
-    else if (argc > 2) {
-        char *longest = argv[1];
+int main(int argc, char* argv[]) {
+        
+        char *longest = "";
         for (int i = 1; i < argc; i++) {
                 if (strlen(argv[i]) > strlen(longest)) {
                     longest = argv[i];
                 }
         }
-        printf(longest);
-    }
-
+        puts(longest);
     return EXIT_SUCCESS;
 }
